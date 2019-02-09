accd39a2cc00b7fa990bd6006796daa4d3efdff3
SEUNAGBEYE <agbeyeseun1@gmail.com>
2019-02-09 23:35:40 +0100

    feat(flight): gets a flight reservations
    - add query to get a specific flight reservation for a specific day
    
    [Finishes #163181353]

--- flight/urls.py
+++ flight/urls.py
@@ -3,10 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 from django.conf.urls import url
 
-from .views import FlightListCreate, FlightRetrieveUpdateDestroyAPIView
+from .views import (
+    FlightListCreate,
+    FlightRetrieveUpdateDestroyAPIView,
+    FlightReservation
+)
 
 app_name = 'flight'
 urlpatterns = [
     url(r'^$', FlightListCreate.as_view()),
     url(r'^(?P<id>\d+)$', FlightRetrieveUpdateDestroyAPIView.as_view()),
+    url(r'^(?P<id>\d+)/reservations', FlightReservation.as_view()),
 ]
