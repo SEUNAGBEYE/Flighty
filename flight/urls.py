911ef07951eaab2b18c6079abe9aecef748c97ea
SEUNAGBEYE <agbeyeseun1@gmail.com>
2019-02-09 20:47:06 +0100

    feat(flight): get a flight
    - adds an endpoint to get a single flights
    
    [Finishes #163844554]

--- flight/urls.py
+++ flight/urls.py
@@ -3,9 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 from django.conf.urls import url
 
-from .views import FlightListCreate
+from .views import FlightListCreate, FlightRetrieveUpdateDestroyAPIView
 
 app_name = 'flight'
 urlpatterns = [
     url(r'^$', FlightListCreate.as_view()),
+    url(r'^(?P<id>\d+)$', FlightRetrieveUpdateDestroyAPIView.as_view()),
 ]
