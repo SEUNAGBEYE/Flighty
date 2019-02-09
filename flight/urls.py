ad64f2f90a83d516a604c685e52e63e32d1415cd
SEUNAGBEYE <agbeyeseun1@gmail.com>
2019-02-09 20:41:08 +0100

    feat(flights): get all flights
    - adds an endpoint to get all flights
    
    [Finishes #163844540]

--- flight/urls.py
+++ flight/urls.py
@@ -5,7 +5,6 @@ from django.conf.urls import url
 
 from .views import FlightListCreate
 
-
 app_name = 'flight'
 urlpatterns = [
     url(r'^$', FlightListCreate.as_view()),
--- flight/views.py
+++ flight/views.py
@@ -17,6 +17,16 @@ from .permissions import IsAdminUserOrReadOnly
 class FlightListCreate(APIView):
     permission_classes = (IsAdminUserOrReadOnly,)
     serializer_class = FlightSerializer
+
+    def get(self, request, *args, **kwargs):
+        """Gets all flights"""
+        # There is nothing to validate or save here. Instead, we just want the
+        # serializer to handle turning our `User` object into something that
+        # can be JSONified and sent to the client.
+
+        response= Flight.objects.all()
+        serializer = self.serializer_class(response, many=True)
+        return Response({'data': serializer.data}, status=status.HTTP_200_OK)
     
     def post(self, request, *args, **kwargs):
         """Creates flights"""
